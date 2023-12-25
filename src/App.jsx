eeff0c0b0cd5783f6a98fb92c228f7e4d80fed3e
eaglefree007 <75333835+eaglefree007@users.noreply.github.com>
2023-12-25 21:45:30 +0530

    update app.jsx
    link upates on submit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       </div>
       <div className="">
         <input type="text" placeholder='Enter Your Username' className="mx-auto flex p-2 rounded-md custom-placeholder placeholder:font-bold  border-black border-2" />
-        <a href="https://www.w3schools.com">
+        <a href="https://www.dikapp.com/#/pages/register/register?id=378810">
           <button type="submit" className="px-4 py-2 rounded-lg mb-2 flex justify-center items-center mx-auto my-2 bg-blue-300 hover:bg-blue-400">SUBMIT</button>
         </a>
         <p className="flex justify-center text-2xl font-bold ">How to avail this offer 🤔</p>
