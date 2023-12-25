e97b6fc1a9428287f3e1551c82ec545582da22d2
eaglefree007 <75333835+eaglefree007@users.noreply.github.com>
2023-12-25 21:16:57 +0530

    Update App.jsx
    total update.

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,13 @@ function App() {
       <div className=" flex justify-center">
         <img src={img1} alt="" classname=" mx-auto"/>
       </div>
-      <span className="flex justify-center text-2xl font bold">Free Follower's Offer ...</span>
-      <div className="w-2/3 md:2/4">
-        <img src={img2} alt="" className="w-100 mx-auto" />
+      <span className="flex justify-center text-2xl font bold">🫰Free Follower's Offer🫰</span>
+      <div className="flex justify-center mx-auto w-[100%] md:w-[50%] lg:w-[25%]">
+        <img src={img2} alt="" className="w-96 mx-auto" />
       </div>
       <div className="">
-        {/* <span className=""> */}
-        <input type="text" placeholder='Enter Your Username' className="mx-auto flex p-2 rounded-md custom-placeholder placeholder:font-bold" />
-        <br />
-        <button type="submit" className="px-4 py-2 mb-2 flex justify-center items-center mx-auto bg-blue-300">SUBMIT</button>
-        {/* </span> */}
+        <input type="text" placeholder='Enter Your Username' className="mx-auto flex p-2 rounded-md custom-placeholder placeholder:font-bold  border-black border-2" />
+        <button type="submit" className="px-4 py-2 rounded-lg mb-2 flex justify-center items-center mx-auto my-2 bg-blue-300 hover:bg-blue-400">SUBMIT</button>
         <p className="flex justify-center text-2xl font-bold ">How to avail this offer 🤔</p>
         <ul className=" text-xxl flex flex-col items-center">
           <li className="">Enter your Username and submit.</li>
