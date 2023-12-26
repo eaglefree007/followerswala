8a577ebc4b215fb8a507233f05d4fc6a2149c557
eaglefree007 <75333835+eaglefree007@users.noreply.github.com>
2023-12-26 11:09:41 +0530

    Update App.jsx
    button 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,14 @@ function App() {
       <div className="">
         <input type="text" placeholder='Enter Your Username' className="mx-auto flex p-2 rounded-md custom-placeholder placeholder:font-bold  border-black border-2" />
         <a href="https://www.dikapp.com/#/pages/register/register?id=378810">
-          <button type="submit" className="px-4 py-2 rounded-lg mb-2 flex justify-center items-center mx-auto my-2 bg-blue-300 hover:bg-blue-400">SUBMIT</button>
+          <button type="submit" className="px-4 py-2 rounded-lg mb-2 flex justify-center items-center mx-auto my-2 bg-blue-300 hover:bg-blue-400">Link 1</button>
+        </a>
+        <a href="https://www.cargoconsoltd.com/#/pages/login/login?id=JupJpJ">
+          <button type="submit" className="px-4 py-2 rounded-lg mb-2 flex justify-center items-center mx-auto my-2 bg-blue-300 hover:bg-blue-400">Link 2</button>
         </a>
         <p className="flex justify-center text-2xl font-bold ">How to avail this offer 🤔</p>
         <ul className=" text-xxl flex flex-col items-center">
-          <li className="">Enter your Username and submit.</li>
+          <li className="">Enter your Username and Hit link button.</li>
           <li className="">You will be redirected to merchant page.</li>
           <li className="">Register and done.</li>
         </ul>
